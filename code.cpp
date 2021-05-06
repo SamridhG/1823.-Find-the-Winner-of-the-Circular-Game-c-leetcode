38dc5bfa064ee544458ac11e3f09386468905f41
Samridh Gupta <63790684+SamridhG@users.noreply.github.com>
2021-05-06 15:37:07 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int>A;
+        int i=1;
+       while(i<=n)
+        {
+            A.push_back(i);
+            i++;
+        }
+        int t=0;
+        while(A.size()!=1)
+        {
+            i=1;
+           
+            while(i<k)
+            {
+                 if(t>=A.size())// checking wheather index not overflow before increasing 
+                {
+                    t=0;
+                }
+                t++;
+                 if(t>=A.size())// checking wheather index not overflow after increasing 
+                {
+                    t=0;
+                }
+                i++;
+            }
+            A.erase(A.begin()+t);
+             
+        }
+        return A[0];
+    }
+};
